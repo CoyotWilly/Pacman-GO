332397591aa3a60f56e6915d23c42771e4716c05
Coyot Willy <coyotwilly@gmail.com>
2024-04-05 19:57:04 +0200

    Model type init

---
+++ src/model/sprite.go
@@ -0,0 +1,8 @@
+package model
+
+type Sprite struct {
+	X     int
+	Y     int
+	XInit int
+	YInit int
+}
---
+++ src/model/unit.go
@@ -0,0 +1,8 @@
+package model
+
+type MazeCharacter struct {
+	Row  int
+	Col  int
+	Line string
+	Char int32
+}
